bb71ca7ab349ba6fb924d4043fbae244ca81a1da
rhaloubi <redahaloubi8@gmail.com>
2025-02-10 18:12:28 +0100

    task route config

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const sequelize = require('./config/database');
 const authRoutes = require('./routes/auth');
 const accountRoutes = require('./routes/account');
+const taskRoutes = require('./routes/tasks');
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/accounts', accountRoutes);
+app.use('/api/tasks', taskRoutes);
 
 // Database sync and server start
 const PORT = process.env.PORT || 3000;
