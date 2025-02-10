4d3317c85afae265095c93168f15c406162a3e2a
rhaloubi <redahaloubi8@gmail.com>
2025-02-10 18:11:29 +0100

    add task model

---
+++ models/task.js
@@ -0,0 +1,45 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const User = require('./user');
+
+const Task = sequelize.define('Task', {
+  id: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+    defaultValue: DataTypes.UUIDV4
+  },
+  id_user: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: User,
+      key: 'id'
+    }
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  type: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  task: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  status: {
+    type: DataTypes.ENUM('TODO', 'IN PROGRESS', 'DONE','Canceled','Backlog'),
+    defaultValue: 'TODO'
+  },
+  priority: {
+    type: DataTypes.ENUM('LOW', 'MEDIUM', 'HIGH'),
+    defaultValue: 'MEDIUM'
+  },
+  team: {
+    type: DataTypes.STRING,
+    allowNull: true
+  }
+});
+
+module.exports = Task;
